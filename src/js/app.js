b85ff28c915a04337b037b1bc1f224f7ebf527ff
Christoph Martel <chris@codeways.org>
2015-05-15 23:17:09 +0200

    working proof of concept
    has some issues with googles text/html response which has to be
    interpreted as jsonp to avoid cross-domain-error.

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,18 @@
-console.log("Hello Worldz");
+$(document).ready(function() {
+  $('#gform-submit').click(function(event) {
+    var formData = $('#gform').serializeArray();
+    var formJson = {};
+    _.forEach(formData, function(n) {
+      formJson[n['name']] = n['value'];
+    })
+  $.ajax({
+    method: "POST",
+    url: 'https://docs.google.com/forms/d/1KEhf7Bu9HxgOcEzCvgpBEhsekkvg8j-bWNsiu1fRC3w/formResponse',
+    data: formJson,
+    dataType: 'jsonp',
+    contentType: 'application/json'
+  })
+    .done()
+    .fail();
+  });
+});
